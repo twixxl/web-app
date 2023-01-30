f4143e6ce12d2dcb72268361667036b015f47ff5
SF180 <sheldon08ferns@gmail.com>
2023-01-30 17:46:13 +0300

    did alot of improvements

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,8 @@
-import React from 'react'
+/*global google*/
+import React from "react";
 import { IconName } from "react-icons/bs";
+import { GoogleLogin } from "react-google-login";
+import { gapi } from "gapi-script";
 const Login = () => {
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden login-page">
@@ -31,7 +34,7 @@ const Login = () => {
           </div>
         </form>
         <div className="relative flex items-center justify-center w-full mt-6 border border-t">
-          <div className="absolute px-5 or">Or</div>
+          <div className="absolute px-5 or">OR</div>
         </div>
         <div className="flex mt-4 gap-x-2">
           <button
@@ -57,16 +60,6 @@ const Login = () => {
             </svg>{" "}
             <label>GitHub</label>
           </button>
-          <button className="social flex items-center justify-center w-full p-2 border border-gray-600 rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-              className="w-5 h-5 fill-current"
-            >
-              <path d="M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z"></path>
-            </svg>{" "}
-            <label>Twitter</label>{" "}
-          </button>
         </div>
 
         <p className="mt-8 text-xs font-light text-center text-white-700">
@@ -79,6 +72,6 @@ const Login = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
